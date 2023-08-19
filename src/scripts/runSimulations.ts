841dc5cb7395b2288ff2ea0873245993ff2a044f
Holden McGinnis <holdenjmcginnis@gmail.com>
2023-08-18 22:45:25 -0400

    update runSimulations to only conditionally show delta from prior week

--- src/scripts/runSimulations.ts
+++ src/scripts/runSimulations.ts
@@ -8,10 +8,11 @@ import { getTeamAndScheduleData, currentWeek, currentYear } from './utils';
 // ts-node src/scripts/runSimulations.ts
 
 // simulate the season, write the results to the file system and log the results in a table
-// TODO: make this not assume the previous week exists
 
 // Import file containing team and schedule data
 const previousWeek = currentWeek - 1;
+const includeChangeWeekOverWeek = false;
+
 const { teams: originalTeams, schedule: originalSchedule } = getTeamAndScheduleData();
 
 // Simulate the season multiple times
@@ -33,9 +34,12 @@ const simulateAndLogResults = (params: {
   const outputFilePath = path.join(__dirname, `../data/simulationResults/${currentYear}-${currentWeek}.json`);
   fs.writeFileSync(outputFilePath, JSON.stringify(currentWeekSimulationResults, null, 2), 'utf8');
 
-  // note assumes same simulation count as current week (for now)
-  const previousWeekFilePath = path.join(__dirname, `../data/simulationResults/${currentYear}-${previousWeek}.json`);
-  const previousWeekSimulationResults = JSON.parse(fs.readFileSync(previousWeekFilePath, 'utf8'));
+  let previousWeekSimulationResults;
+  if (includeChangeWeekOverWeek) {
+    // note assumes same simulation count as current week (for now)
+    const previousWeekFilePath = path.join(__dirname, `../data/simulationResults/${currentYear}-${previousWeek}.json`);
+    previousWeekSimulationResults = JSON.parse(fs.readFileSync(previousWeekFilePath, 'utf8'));
+  }
 
   const simulationCount = currentWeekSimulationResults[Object.keys(currentWeekSimulationResults)[0]].numSeasons;
 
@@ -63,15 +67,18 @@ const simulateAndLogResults = (params: {
 
   sortedTeamList.forEach((entity, index) => {
     const teamStats = currentWeekSimulationResults[entity.name];
-    const previousWeekStats = previousWeekSimulationResults[entity.name];
 
-    const playoffDelta = teamStats.playoffAppearances - previousWeekStats.playoffAppearances;
+    let playoffDelta;
+    if (includeChangeWeekOverWeek) {
+      const previousWeekStats = previousWeekSimulationResults[entity.name];
+      playoffDelta = teamStats.playoffAppearances - previousWeekStats.playoffAppearances;
+    }
 
     resultsList.push([
       `${index + 1}`,
       entity.name,
       getStringPercentage(teamStats.playoffAppearances),
-      getStringPercentage(playoffDelta),
+      ...(includeChangeWeekOverWeek ? [getStringPercentage(playoffDelta)] : []),
       getStringPercentage(teamStats.rankings['1']),
       getStringPercentage(teamStats.rankings['2']),
       getStringPercentage(teamStats.rankings['3']),
